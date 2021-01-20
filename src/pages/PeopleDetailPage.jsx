17e04f9adcf3f1d0d8bf88c1d5caf7b4fbcfdf9f
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2021-01-20 12:01:02 +0100

    Start on peopleDetailPage and render attributes

--- src/pages/PeopleDetailPage.jsx
+++ src/pages/PeopleDetailPage.jsx
@@ -1,5 +1,26 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import PeopleDetail from "../components/PeopleDetail"
 
-export default function PeopleDetailPage() {
-	return <div>PeopleDetailPage</div>
+export default function PeopleDetailPage(props) {
+	// console.log(props.match.params.id)
+	const id = props.match.params.id
+	const [person, setPerson] = useState(null)
+
+	function getPerson() {
+		const url = `https://swapi.dev/api/people/${id}/`
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => setPerson(data))
+	}
+
+	useEffect(() => {
+		getPerson()
+	}, [])
+
+	return (
+		<div>
+			PeopleDetailPage
+			{person && <PeopleDetail person={person} />}
+		</div>
+	)
 }
