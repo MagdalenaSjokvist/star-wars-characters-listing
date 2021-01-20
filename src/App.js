6015fcd39b8c5d3d72c76e817684141930c4e2e0
MagdalenaSjokvist <magdalena.sjokvist@gmail.com>
2021-01-20 11:23:16 +0100

    Create Routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react"
+import { Switch, Route } from "react-router-dom"
+import PeopleDetailPage from "./pages/PeopleDetailPage"
+import PeopleListPage from "./pages/PeopleListPage"
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+	return (
+		<div>
+			<Switch>
+				<Route path="/people/:id" component={PeopleDetailPage} />
+				<Route path="/people" component={PeopleListPage} />
+			</Switch>
+		</div>
+	)
 }
 
-export default App;
+export default App
